ec87c84eb05c3ae4127456f0e3315d4f5d5e2e42
Daniel Reigada <daniel@codacy.com>
2018-07-12 15:20:32 +0100

    Add patterns classes

---
+++ lib/codacy/bundler_audit/patterns.rb
@@ -0,0 +1,56 @@
+module Codacy
+  module BundlerAudit
+    module Patterns
+
+      class Pattern
+        attr_reader :filename, :pattern_id
+
+        def initialize(pattern_id, filename)
+          @pattern_id = pattern_id
+          @filename = filename
+        end
+      end
+
+      class UnpatchedGem < Pattern
+        PATTERN_ID = "Insecure Dependency".freeze
+
+        # @param [Bundler::Audit::Scanner::UnpatchedGem] issue
+        def initialize(issue, filename)
+          super(PATTERN_ID, filename)
+          @issue = issue
+          @filename = filename
+        end
+
+        def message
+          # TODO
+          'UnpatchedGem'
+        end
+
+        def line
+          # TODO
+          2
+        end
+      end
+
+      class InsecureSource < Pattern
+        PATTERN_ID = "Insecure Source".freeze
+
+        # @param [Bundler::Audit::Scanner::InsecureSource] issue
+        def initialize(issue, filename)
+          super(PATTERN_ID, filename)
+          @issue = issue
+        end
+
+        def message
+          # TODO
+          'InsecureSource'
+        end
+
+        def line
+          # TODO
+          1
+        end
+      end
+    end
+  end
+end
