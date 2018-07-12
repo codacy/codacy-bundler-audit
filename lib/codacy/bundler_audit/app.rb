aa44bd40f5635889eadf589fa89b0d331aefe959
Daniel Reigada <daniel@codacy.com>
2018-07-12 17:50:13 +0100

    Implement message and line methods for patterns

--- lib/codacy/bundler_audit/app.rb
+++ lib/codacy/bundler_audit/app.rb
@@ -7,11 +7,13 @@ module Codacy
     class App
       # @param [Codacy::BundlerAudit::CodacyConfig] config
       def run(config)
+        files_read = Hash.new {|h, key| h[key] = read_file_lines(key).to_a}
+
         config.files
             .select {|file| File.basename(file) == 'Gemfile.lock'}
             .flat_map {|file| run_in_dir(File.expand_path("..", file))
                                   .map {|issue| [issue, file]}.to_a}
-            .map {|issue_file| convert_issue(issue_file[0], issue_file[1]).to_json}
+            .map {|issue_file| convert_issue(issue_file[0], issue_file[1], files_read[issue_file[1]]).to_json}
       end
 
       # @param [String] source_root
@@ -22,12 +24,16 @@ module Codacy
         end
       end
 
-      def convert_issue(issue, filename)
+      def read_file_lines(file)
+        File.open(file).each_line
+      end
+
+      def convert_issue(issue, filename, file_lines)
         case issue
         when Bundler::Audit::Scanner::UnpatchedGem
-          Codacy::BundlerAudit::Patterns::UnpatchedGem.new(issue, filename)
+          Codacy::BundlerAudit::Patterns::UnpatchedGem.new(issue, filename, file_lines)
         when Bundler::Audit::Scanner::InsecureSource
-          Codacy::BundlerAudit::Patterns::InsecureSource.new(issue, filename)
+          Codacy::BundlerAudit::Patterns::InsecureSource.new(issue, filename, file_lines)
         else
           #TODO handle error
         end
--- lib/codacy/bundler_audit/patterns.rb
+++ lib/codacy/bundler_audit/patterns.rb
@@ -3,63 +3,66 @@ module Codacy
     module Patterns
 
       class Pattern
-        attr_reader :filename, :pattern_id
+        attr_reader :file_path, :pattern_id
 
-        def initialize(pattern_id, filename)
+        def initialize(pattern_id, file_path, file_lines, line_regex, regex_match_comp)
           @pattern_id = pattern_id
-          @filename = filename
+          @file_path = file_path
+          @file_lines = file_lines
+          @line_regex = line_regex
+          @regex_match_comp = regex_match_comp
         end
 
         def to_json(*a)
           {
-              filename: @filename,
+              file_path: @file_path,
               message: self.message,
               patternId: @pattern_id,
               line: self.line
           }.to_json(*a)
         end
+
+        def line
+          @line_number ||= begin
+            @file_lines.find_index do |line|
+              (match = @line_regex.match(line)) && match['comp'] == @regex_match_comp
+            end + 1
+          end
+        end
       end
 
       class UnpatchedGem < Pattern
         PATTERN_ID = "Insecure Dependency".freeze
+        LINE_REGEX = /^\s*(?<comp>\S+) \([\S.]+\)$/.freeze
 
         # @param [Bundler::Audit::Scanner::UnpatchedGem] issue
-        def initialize(issue, filename)
-          super(PATTERN_ID, filename)
+        # @param [String] file_path
+        def initialize(issue, file_path, file_lines)
+          super(PATTERN_ID, file_path, file_lines, LINE_REGEX, issue.gem.name)
           @issue = issue
-          @filename = filename
+          @file_path = file_path
         end
 
         def message
-          # TODO
-          'UnpatchedGem'
-        end
-
-        def line
-          # TODO
-          2
+          "#{@issue.advisory.title} (#{@issue.advisory.id})"
         end
       end
 
       class InsecureSource < Pattern
         PATTERN_ID = "Insecure Source".freeze
+        LINE_REGEX = /^\s*remote: (?<comp>\S+)$/.freeze
 
         # @param [Bundler::Audit::Scanner::InsecureSource] issue
-        def initialize(issue, filename)
-          super(PATTERN_ID, filename)
+        def initialize(issue, file_path, file_lines)
+          super(PATTERN_ID, file_path, file_lines, LINE_REGEX, issue.source)
           @issue = issue
         end
 
         def message
-          # TODO
-          'InsecureSource'
-        end
-
-        def line
-          # TODO
-          1
+          "Insecure Source URI found: #{@issue.source}"
         end
       end
+
     end
   end
 end
