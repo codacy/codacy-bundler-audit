65162651f69fe188bd4ad5a8fed3b4d028dba531
Daniel Reigada <daniel@codacy.com>
2018-07-12 11:22:20 +0100

    Move codacy config

---
+++ lib/codacy/config.rb
@@ -0,0 +1,39 @@
+require 'json'
+
+module Codacy
+  class Config
+    attr_reader :json, :files, :tools
+
+    def initialize(json)
+      @json = json
+      @files = @json["files"]
+      @tools = @json["tools"].map {|tool| Tool.new(tool)}
+    end
+
+    class << Config
+      def parse(raw_json)
+        Config.new(JSON.parse(raw_json))
+      end
+    end
+  end
+end
+
+class Tool
+  attr_reader :json, :name, :patterns
+
+  def initialize(json)
+    @json = json
+    @name = @json["name"]
+    @patterns = @json["patterns"].map {|pattern| Pattern.new(pattern)}
+  end
+end
+
+class Pattern
+  attr_reader :json, :pattern_id, :parameters
+
+  def initialize(json)
+    @json = json
+    @pattern_id = @json["patternId"]
+    @parameters = @json["parameters"]
+  end
+end
