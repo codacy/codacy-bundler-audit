36b9c740ff9621a7e6f11e9cf20d4cd7321e51fd
stefanvacareanu7 <91726880+stefanvacareanu7@users.noreply.github.com>
2023-10-10 17:02:10 +0300

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,5 @@ lib = File.expand_path(File.join(File.dirname(__FILE__), "../lib"))
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 require 'rspec/core'
-require 'codacy-coverage'
 
 Codacy::Reporter.start
